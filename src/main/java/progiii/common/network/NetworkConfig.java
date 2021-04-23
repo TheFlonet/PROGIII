a24e608ef9b836a2c01155760ff3e6b00bc29cfd
Mario <flonet.altervista@gmail.com>
2021-04-23 09:31:16 +0200

    small changes

--- src/main/java/progiii/common/network/NetworkConfig.java
+++ src/main/java/progiii/common/network/NetworkConfig.java
@@ -3,9 +3,9 @@ package progiii.common.network;
 /**
  *
  * Raccoglie le configurazioni per la comunicazione di rete (IP, porta e pull period)
- * 	NB il pull period è basso per (5 secondi) per motivi didattici
- * 	Una implementazione reale dovrebbe avere un delay maggiore
- * 	Questo eviterebbe un sovraccarico di richieste dai client
+ * NB il pull period è basso per (5 secondi) per motivi didattici
+ * Una implementazione reale dovrebbe avere un delay maggiore (gmail aggiorna automaticamente i client ogni 15 minuti)
+ * Questo eviterebbe un sovraccarico di richieste dai client
  */
 public class NetworkConfig {
     public static final String REMOTE_IP = "localhost";
